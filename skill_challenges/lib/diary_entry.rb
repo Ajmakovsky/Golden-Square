9218a32b041c1d695570f13d5929a8033f3e5037
Abigail <ajmakovsky@gmail.com>
2023-01-20 11:46:44 +0000

    Diary_Entry Class exercise completed

--- skill_challenges/lib/diary_entry.rb
+++ skill_challenges/lib/diary_entry.rb
@@ -2,6 +2,7 @@ class DiaryEntry
   def initialize(title, contents) # title, contents are strings
     @title = title
     @contents = contents
+    @starting_index = 0 
   end
 
   def title
@@ -13,34 +14,33 @@ class DiaryEntry
   end
 
   def count_words
-    return @contents.split.length # Returns the number of words in the contents as an integer
+    @contents_length = @contents.split.length # Returns the number of words in the contents as an integer
   end
 
-  def reading_time(wpm)
-    return count_words / wpm
-    
-    # wpm is an integer representing the number of words the
-    # user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # for the contents at the given wpm.
+  def reading_time(wpm) 
+    # wpm is an integer representing the number of words the user can read per minute; 
+    # Returns an integer representing an estimate of the reading time in minutes for the contents
+    return count_words / wpm.to_f.ceil
   end
 
   def reading_chunk(wpm, minutes) 
     contents_arr = @contents.split
     total_words = wpm * minutes
+
+    #starting_index = @starting_index
+    ending_index = @starting_index + total_words 
+
+    @starting_index = 0 if (@starting_index + total_words) > contents_arr.length 
+
+    text_chunk = contents_arr[@starting_index,total_words].join(" ")
+      @starting_index = ending_index
+      return text_chunk
     
-    #chunk_text = text_array.join[0...total_words]
-    # if next chunk starts at/ the end of the contents, reset to 0
-    starting_index = 0 if starting_index >= contents_arr.length
-    ending_index = starting_index + total_words #Give the end index of chunk to read
-    text_chunk = contents_arr[starting_index...ending_index].join(" ")
-    starting_index = ending_index
-    
-    return  text_chunk
-    # `wpm` is an integer representing the number
-    # of words the user can read per minute
-    # `minutes` is an integer representing the
-    # number of minutes the user has to read
+
+      # `wpm` is an integer representing the number
+      # of words the user can read per minute
+     # `minutes` is an integer representing the
+     # number of minutes the user has to read
     # Returns a string with a chunk of the contents that the user could read
     # in the given number of minutes.
     # If called again, `reading_chunk` should return the next chunk, skipping
@@ -48,3 +48,8 @@ class DiaryEntry
     # The next call after that it should restart from the beginning.
   end
 end
+
+#diary_entry1 = DiaryEntry.new("Today","Lorem Ipsum is simply dummy text of the printing and typesetting industry")
+#puts diary_entry1.reading_chunk(2,2)
+#puts diary_entry1.reading_chunk(2,2)
+
--- skill_challenges/spec/diary_entry_spec.rb
+++ skill_challenges/spec/diary_entry_spec.rb
@@ -15,10 +15,14 @@ describe DiaryEntry do
     it "Returns an integer representing an estimate of the reading time in minutes for the contents at the given wpm." do
         diary_entry1 = DiaryEntry.new("Today","Good day " * 10)
         expect(diary_entry1.reading_time(1)).to eq 20
+        expect(diary_entry1.reading_time(10)).to eq 2
     end
 
     it "Returns a chunk of text based on WPM and available minutes." do
       diary_entry1 = DiaryEntry.new("Today","Lorem Ipsum is simply dummy text of the printing and typesetting industry")
+      expect(diary_entry1.reading_chunk(2,2)).to eq "Lorem Ipsum is simply"
+      expect(diary_entry1.reading_chunk(2,2)).to eq "dummy text of the"
+      expect(diary_entry1.reading_chunk(2,2)).to eq "printing and typesetting industry"
       expect(diary_entry1.reading_chunk(2,2)).to eq "Lorem Ipsum is simply"
     end
 end
